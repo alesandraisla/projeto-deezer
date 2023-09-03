d3e0b3063d57a0d4836fe5f5603034645001b3e6
AlesandraIsla <alesandramempis@gmail.com>
2023-09-02 23:19:45 -0300

    Inclusao do conteudo top do perfil

--- src/pages/MoreMusic/MoreMusic.tsx
+++ src/pages/MoreMusic/MoreMusic.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import { Layout } from '../../Layout/Layout';
+import { AboutProfile, ButtonProfile, ContainerMoreMusic, ContentBottom, ContentTop, ImageProfile, ItemNav, ItemsProfile, NavigationPerfil, TitleProfile } from './MoreMusicStyle';
+import avatarIsla from '../../images/avatarIsla.jpeg';
+import { Link } from 'react-router-dom';
 
+const namesNavigationProfile = [
+  {id: 1,  name: 'Destaques', url: '/#'},
+  {id: 2,  name: 'Mais queridas', url: '/#'},
+  {id: 3,  name: 'playlists', url: '/#'},
+  {id: 4,  name: 'Álbuns', url: '/#'},
+  {id: 5,  name: 'Artistas', url: '/#'},
+  {id: 6,  name: 'Histórico de reprodução', url: '/#'},
+  {id: 7,  name: 'Podcasts', url: '/#'},
+  {id: 8,  name: 'Mais', url: '/#'},
 
+]
 export function MoreMusic() {
   return(
     <Layout>
-      <div>More Music </div>
+      <ContainerMoreMusic>
+        <ContentTop>
+          <ImageProfile src={avatarIsla}/>
+          <ItemsProfile>
+            <TitleProfile>
+              Alesandra Isla
+            </TitleProfile>
+            <AboutProfile>
+              <span>
+                34 seguidores
+              </span>
+              <span>
+                77 seguindo
+              </span>
+            </AboutProfile>
+            <ButtonProfile>
+              MINHA MÚSICA - ALEATÓRIO
+            </ButtonProfile>
+          </ItemsProfile>
+        </ContentTop>
+        <ContentBottom>
+          <NavigationPerfil>
+          {namesNavigationProfile.map((item, index) => (
+            <ItemNav key={item.id}> 
+            <Link to={item.url}>
+              {item.name} 
+            </Link>
+            </ItemNav>
+          ))}
+          </NavigationPerfil>
+        </ContentBottom>
+        </ContainerMoreMusic>
     </Layout>
   )
 }
--- src/pages/MoreMusic/MoreMusicStyle.ts
+++ src/pages/MoreMusic/MoreMusicStyle.ts
@@ -1,3 +1,58 @@
 import styled from 'styled-components';
 
-export const ContainerAllMusics = styled.div ``
+export const ContainerMoreMusic = styled.div ``
+
+export const ContentTop = styled.div `
+display: flex;
+padding: 20px;
+`
+
+export const ContentBottom = styled.div ``
+
+export const ImageProfile = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 60px;
+`
+export const ItemsProfile = styled.div`
+  padding-left: 20px;
+`
+
+export const TitleProfile = styled.h3`
+  margin-bottom: 5px;
+`
+
+export const AboutProfile = styled.div`
+  font-size: 12px;
+  padding-bottom: 5px;
+  span {
+    padding-right: 10px;
+  }
+`
+export const ButtonProfile = styled.button`
+display: inline-flex;
+    appearance: none;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    user-select: none;
+    position: relative;
+    white-space: nowrap;
+    vertical-align: middle;
+    outline: transparent solid 2px;
+    outline-offset: 2px;
+    line-height: 1;
+    text-transform: uppercase;
+    color: white;
+    width: auto;
+    background: #ef5466;
+    border: none;
+    border-radius: 20px;
+    padding: 10px;
+    font-size: 12px;
+`
+
+export const NavigationPerfil = styled.div``
+
+export const ItemNav = styled.li``
