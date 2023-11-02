eeb4b1a4885ea035722ce68d7ae7ec3bfbcf05b1
AlesandraIsla <alesandramempis@gmail.com>
2023-11-02 10:40:47 -0300

    Ajuste do nome

---
+++ docs/precache-manifest.c5dd482a4a1ef527d2dd1cee2aa2e885.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "03bc3c5eddb57a7e624b",
+    "url": "/static/js/main.03bc3c5e.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "29ccca17638143ea81d8",
+    "url": "/static/js/2.29ccca17.chunk.js"
+  },
+  {
+    "revision": "fb573bd4075a37a7f80b",
+    "url": "/static/js/runtime~main.fb573bd4.js"
+  },
+  {
+    "revision": "9e75b57d8ab4b0a164439a0fccbb23d6",
+    "url": "/static/media/avatarIsla.9e75b57d.jpeg"
+  },
+  {
+    "revision": "4f25b90e9ae4a7f2f83e62e93cd8e269",
+    "url": "/static/media/logoDeezer.4f25b90e.png"
+  },
+  {
+    "revision": "35c8f7ab797b8678610e3cac8d6eb23b",
+    "url": "/static/media/paramore.35c8f7ab.webp"
+  },
+  {
+    "revision": "dd7a9c9ab321c1ad36693f39bbecf353",
+    "url": "/index.html"
+  }
+];
